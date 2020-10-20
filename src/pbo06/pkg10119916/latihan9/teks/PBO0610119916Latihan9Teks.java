28cea344dba25c355b2878894d120c02a40257ef
DianArpian <>
2020-10-20 19:27:44 +0700

    Teks

---
+++ src/pbo06/pkg10119916/latihan9/teks/PBO0610119916Latihan9Teks.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo06.pkg10119916.latihan9.teks;
+
+/**
+ *
+ * @author 
+ * NAMA     : DIAN ARPIAN
+ * KELAS    : IF10K
+ * NIM      : 10119916 
+ * Deskripsi Program : Teks
+ */
+public class PBO0610119916Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "char:" + tab + "berisi" + c);
+    }
+    
+}
